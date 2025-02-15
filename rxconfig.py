2fbe349c959db430fb7cabdfd38fe2ff482beeb8
Raymond L. <10707034+QutritSystems@users.noreply.github.com>
2025-02-16 03:58:55 +0800

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -1,10 +1,9 @@
 import reflex as rx
-import os
 
 config = rx.Config(
     app_name="todo_app",
     db_url="sqlite:///reflex.db",
     env=rx.Env.PROD,
     backend_host="0.0.0.0",
-    port=8080  # Railway's default port
+    backend_port=8080,
 )
